806f2ed4300939613c2677accc4ae1fc863abc3e
Francis <Francis@DESKTOP-RV27NST>
2025-08-30 20:05:08 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Francis-Portfolio/", // 👈 must have trailing slash
+  base: '/Francis-Portfolio/',
 })
